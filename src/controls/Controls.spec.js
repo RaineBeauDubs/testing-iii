5ccd5f065aa289242239b3ce991357a77e7e58dc
Raine Wallace <rainebeaudubs@gmail.com>
2019-03-13 18:38:52 -0400

    I know it's a simple test, but at least there are a couple passing tests now.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+
+import Controls from './Controls';
+
+describe ('<Controls />', () => {
+  it.skip('should call the toggleLocked function', () => {
+    const toggleLocked = jest.fn();
+    const { getByText } = render(<Controls />)
+
+    fireEvent.click(getByText(/banana/));
+    expect(toggleLocked).toHaveBeenCalled();
+  });
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,21 @@
-// Test away!
+import React from 'react';
+import {render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Controls from '../controls/Controls';
+
+describe('<Display />', () => {
+  it('should render lock button', () => {
+    const { getByText } = render(<Controls />);
+    const lockButton = getByText('Lock Gate');
+
+    expect(lockButton).toBeInTheDocument();
+  })
+
+  it('should render close button', () => {
+    const { getByText } = render(<Controls />);
+    const closeButton = getByText('Close Gate');
+
+    expect(closeButton).toBeInTheDocument();
+  })
+})
